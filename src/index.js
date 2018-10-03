df94e700ffe6a9c9c0ca1231fecd0f18cf6032a8
Anmol Desai <anmoldesai4@gmail.com>
2018-10-03 22:51:34 +1000

    setup redux and reducers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+import { Provider } from 'react-redux'; // this binds the react with redux
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// creates a store
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
