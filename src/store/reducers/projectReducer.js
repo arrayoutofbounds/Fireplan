7fdab3e5359035630da6edb3b1ccddc187fafba4
Anmol Desai <anmoldesai4@gmail.com>
2018-10-03 23:31:36 +1000

    setup redux dummy data and reducer

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,5 +1,9 @@
 const initialState = {
-
+    projects: [
+        {id: '1', title: 'help me find peach', content: 'blah blah blah'},
+        {id: '1', title: 'help me find peach', content: 'blah blah blah'},
+        {id: '1', title: 'help me find peach', content: 'blah blah blah'}
+    ]
 };
 
 const projectReducer = (state=initialState, action) => {
