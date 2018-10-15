317c3d1fa6f0acd8bca82bc708b4dfcb9d1889c2
Anmol Desai <anmoldesai4@gmail.com>
2018-10-16 09:15:08 +1100

    create document in firestore

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux'; // this binds the react with redux
 import thunk from 'redux-thunk';
-import { reduxFirestore, getFirestore } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import { reduxFirestore, getFirestore } from 'redux-firestore';  // firestore bindings for redux
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'; // firebase bindings for redux
 import fbConfig from './config/fbConfig';
 
 const composeEnhancers =
@@ -27,6 +27,6 @@ const enhancer = composeEnhancers(
 
 // creates a store
 const store = createStore(rootReducer,  enhancer);
-
+ 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,6 +2,20 @@ export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make async call to fire store
 
-        dispatch({type: 'CREATE_PROJECT', project});
+        // gives us a reference to the firestore database
+        const firestore = getFirestore(); 
+
+        // async, that returns a promise
+        firestore.collection('projects').add({ 
+            ...project,
+            authorFirstName: 'anmol',
+            authorLastName: 'desai',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({type: 'CREATE_PROJECT', project});
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err })
+        });
     }
 };
