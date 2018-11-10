d56f315f81de8be048ec02daebbffeb6491a2235
Anmol Desai <anmoldesai4@gmail.com>
2018-11-10 21:21:38 +1100

    order the projects and notifications

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -8,7 +8,7 @@ import { Redirect } from 'react-router-dom';
 
 class Dashboard extends Component {
     render(){
-        const { projects, auth } = this.props;
+        const { projects, auth, notifications } = this.props;
         if(!auth.uid){
             return <Redirect to='/signin' />
         }
@@ -19,7 +19,7 @@ class Dashboard extends Component {
                         <ProjectList projects={projects} />
                     </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notifications/>
+                        <Notifications notifications={notifications} />
                     </div>
                 </div>
             </div>
@@ -30,14 +30,17 @@ class Dashboard extends Component {
 const mapStateToProps = (state) => {
     return {
         projects: state.firestore.ordered.projects,
-        auth: state.firebase.auth
+        auth: state.firebase.auth, // get the current user auth
+        notifications: state.firestore.ordered.notifications, // gets the array of ordered notifications
     }
 }
 
+// we are connecting this to redux from the collections and then accessing it here.
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'projects'}
+        { collection: 'projects', orderBy: ['createdAt', 'desc']},
+        { collection: 'notifications', limit: 3, orderBy: ['time', 'desc']}
     ])
 )(Dashboard);
 
